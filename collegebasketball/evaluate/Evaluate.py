df3604c3da40208187a519419c2b12f675110619
pjmartinkus <pjmartinkus@yahoo.com>
2022-02-21 17:14:28 -0600

    Added function to calculate post tourney performance metrics

--- collegebasketball/evaluate/Evaluate.py
+++ collegebasketball/evaluate/Evaluate.py
@@ -188,27 +188,27 @@ def evaluate(train, test, exclude, models, model_names):
 
 def probability_graph(actual, probabilities, model_names=None, start=0.0, stop=0.7, bin_width=0.05, **kwargs):
     """
-        Creates a line plot visualization plotting the predicted probabilities from a model
-        against the fraction of actual upsets for those predictions. The functions splits
-        the predicted probabilities into bins spanning from the given starting and stopping
-        values. Then the fraction of upsets for the games in each bin is calculated and plotted
-        in a line plot. If the `probabilities` argument is a list of lists, the function
-        will calculate those upset percentages for each list of probabilities and plot them
-        in multiple series.
+    Creates a line plot visualization plotting the predicted probabilities from a model
+    against the fraction of actual upsets for those predictions. The functions splits
+    the predicted probabilities into bins spanning from the given starting and stopping
+    values. Then the fraction of upsets for the games in each bin is calculated and plotted
+    in a line plot. If the `probabilities` argument is a list of lists, the function
+    will calculate those upset percentages for each list of probabilities and plot them
+    in multiple series.
 
-        The goal is to provide a visualization to help determine if the probability given by
-        the classifier is actually representative of the probability the classifier is correct.
+    The goal is to provide a visualization to help determine if the probability given by
+    the classifier is actually representative of the probability the classifier is correct.
 
-        Args:
-            actual(List): A list like object of the correct result for a set of feature vectors.
-            probabilities(List): A list (or list of lists if plotting multiple series) of predicted
-                                 probabilities for the same games as the `actual` input list.
-            model_names(List): The names of each series of probabilities if plotting multiple series
-            start(Float): The start of the range of probabilities to cover. Should be between 0 and 1.
-            stop(Float): The end of the range of probabilities to cover. Should be between 0 and 1.
-            bin_width(Float): The width of each bin.
-            kwargs: Key word arguments to pass on to the figure functions from matplotlib.
-        """
+    Args:
+        actual(List): A list like object of the correct result for a set of feature vectors.
+        probabilities(List): A list (or list of lists if plotting multiple series) of predicted
+                             probabilities for the same games as the `actual` input list.
+        model_names(List): The names of each series of probabilities if plotting multiple series
+        start(Float): The start of the range of probabilities to cover. Should be between 0 and 1.
+        stop(Float): The end of the range of probabilities to cover. Should be between 0 and 1.
+        bin_width(Float): The width of each bin.
+        kwargs: Key word arguments to pass on to the figure functions from matplotlib.
+    """
     num_bins = int((stop - start) / bin_width) + 1
     upset_pcts = list()
     for probs in probabilities:
@@ -249,3 +249,85 @@ def get_stats(data, model_name):
     accuracy = accuracy_score(data['Label'], data['Prediction'])
 
     return [model_name, precision, recall, f1, auc, brier, accuracy]
+
+
+def post_tournament_eval(predictions, scores, kenpom):
+    """
+    Calculates some metrics once the NCAA Tournament is over to evaluate how the model
+    performed. The metrics include the accuracy of the model over each round of the
+    tournament.
+
+    Args:
+        predictions(DataFrame): A pandas DataFrame of predictions.
+        scores(DataFrame): A pandas DataFrame of game score data from the tournament.
+        kenpom(DataFrame): A pandas DataFrame of Kenpom data used to determine who was favored
+                      in each game.
+
+    Returns:
+        A new pandas DataFrame that includes accuracy metrics for each round.
+
+    Raises:
+        AssertionError: If any of the input dataframes are not of type pandas DataFrame.
+    """
+
+    # Check that input arguments are actually dataframes
+    if not isinstance(predictions, pd.DataFrame):
+        raise AssertionError('Input argument predictions must be a pandas DataFrame.')
+    if not isinstance(scores, pd.DataFrame):
+        raise AssertionError('Input argument scores must be a pandas DataFrame.')
+    if not isinstance(kenpom, pd.DataFrame):
+        raise AssertionError('Input argument kenpom must be a pandas DataFrame.')
+
+    # Join kenpom and scores
+    data = pd.merge(scores, kenpom[['Team', 'Rank']], left_on='Home', right_on='Team', sort=False)
+    data = pd.merge(data, kenpom[['Team', 'Rank']], left_on='Away', right_on='Team', suffixes=('_Home', '_Away'),
+                    sort=False)
+
+    # Split data by round
+    round_names = ['First Round', 'Second', 'Regional Semifinal', 'Regional Final', 'National Semifinal',
+                   'National Final']
+    round_idx = [(0, 32), (32, 48), (48, 56), (56, 60), (60, 62), (62, 63)]
+
+    results = list()
+    for (n, i) in zip(round_names, round_idx):
+        preds = predictions[i[0]:i[1]]
+        games = data[data['Tournament'].apply(lambda x: n in x)]
+
+        # Create some intermediate data sets
+        all_teams = pd.concat([games['Home'], games['Away']])
+        all_predicted_teams = pd.concat([preds['Favored'], preds['Underdog']])
+        winners = pd.concat([games[games['Home_Score'] > games['Away_Score']]['Home'],
+                             games[games['Home_Score'] < games['Away_Score']]['Away']])
+        underdog = pd.concat([games[games['Rank_Home'] > games['Rank_Away']]['Home'],
+                              games[games['Rank_Home'] < games['Rank_Away']]['Away']])
+
+        # Calculate Metrics
+        has_pred = preds[preds['Predicted Winner'].isin(all_teams)]
+        correct = preds[preds['Predicted Winner'].isin(winners)]
+        upsets = underdog[underdog.isin(winners)]
+        upsets_predicted = preds[preds['Predicted Winner'] == preds['Underdog']]
+        has_pred_upset = upsets_predicted[upsets_predicted['Predicted Winner'].isin(all_teams)]
+        has_actual_upset = upsets[upsets.isin(all_predicted_teams)]
+        correct_upsets = correct[correct['Predicted Winner'].isin(underdog)]
+        correct_predicted_upsets = upsets_predicted[upsets_predicted['Predicted Winner'].isin(winners)]
+
+        results.append([len(games), len(has_pred), len(correct), len(upsets),
+                        len(upsets_predicted), len(has_actual_upset),
+                        len(has_pred_upset), len(correct_upsets), len(correct_predicted_upsets)])
+
+    # Add in total row
+    cols = ['Games', 'Games Contained Predicted Winner', 'Correct Predictions', 'Total Upsets',
+            'Upsets Predicted', 'Games Containing Actual Upset Winner',
+            'Games Containing Predicted Upset Winner', 'Correct Upsets', 'Correct Predicted Upsets']
+    results = pd.DataFrame(results, columns=cols)
+    results = pd.concat([results, pd.DataFrame(results.sum()).transpose()])
+
+    # Add in metric columns
+    results['Total Accuracy'] = results['Correct Predictions'] / results['Games']
+    results['Upset Precision'] = results['Correct Predicted Upsets'] / results['Upsets Predicted']
+    results['Upset Recall'] = results['Correct Upsets'] / results['Total Upsets']
+    results['Adj Accuracy'] = results['Correct Predictions'] / results['Games Contained Predicted Winner']
+    results['Adj Upset Precision'] = results['Correct Predicted Upsets'] / \
+                                     results['Games Containing Predicted Upset Winner']
+    results['Adj Upset Recall'] = results['Correct Upsets'] / results['Games Containing Actual Upset Winner']
+    return results
